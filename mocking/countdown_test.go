af9219c2ac2a1fb3ad9aa66d41f3b13a23e23656
tardhiansyah <tardhians@gmail.com>
2025-05-26 14:50:00 +0700

    mocking countdown

---
+++ mocking/countdown_test.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
+
+	Countdown(buffer, spySleeper)
+
+	got := buffer.String()
+	want := "3\n2\n1\nGo!"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, got %d want %d", spySleeper.Calls, 3)
+	}}
