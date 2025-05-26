1f0010738ec2e74c1e505241ba779cb54507f001
tardhiansyah <tardhians@gmail.com>
2025-05-26 15:58:04 +0700

    custom iterator

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"iter"
 	"os"
 	"time"
 )
@@ -54,8 +55,18 @@ func (s *SpyTime) Sleep(duration time.Duration) {
 	s.durationSlept = duration
 }
 
+func countDownFrom(start int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for i := start; i > 0; i-- {
+			if !yield(i) {
+				return
+			}
+		}
+	}
+}
+
 func Countdown(out io.Writer, sleeper Sleeper) {
-	for i := countdownStart; i > 0; i-- {
+	for i := range countDownFrom(3) {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
