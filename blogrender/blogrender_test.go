a025ca55c6142a179f06fbd6820d89a39300c005
tardhiansyah <tardhians@gmail.com>
2025-06-01 10:57:53 +0700

    modified approved files for blogrender

---
+++ blogrender/blogrender_test.go
@@ -0,0 +1,30 @@
+package blogrenderer_test
+
+import (
+	"bytes"
+	"testing"
+
+	approvals "github.com/approvals/go-approval-tests"
+	blogrenderer "github.com/tardhiansyah/blogrenderer"
+)
+
+func TestRender(t *testing.T) {
+	var (
+		aPost = blogrenderer.Post{
+			Title:       "Hello World",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	t.Run("it converts a single post into HTML", func(t *testing.T) {
+		buf := bytes.Buffer{}
+
+		if err := blogrenderer.Render(&buf, aPost); err != nil {
+			t.Fatal(err)
+		}
+
+		approvals.VerifyString(t, buf.String())
+	})
+}
---
+++ blogrender/go.mod
@@ -0,0 +1,5 @@
+module github.com/tardhiansyah/blogrenderer
+
+go 1.24.3
+
+require github.com/approvals/go-approval-tests v1.5.1 // indirect
